1274c8b789597acbeed52af53b4c730f4323f9e1
Emeka Manuel <marrnuel123@gmail.comm>
2025-06-08 18:23:57 +0100

    chore(): cleanup

--- src/app/about/index.tsx
+++ src/app/about/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useState, useEffect, useMemo } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Play } from "lucide-react";
